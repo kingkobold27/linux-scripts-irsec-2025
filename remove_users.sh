3f73b489bd820fbf115e8e2c1ccd4bf380a4adb6
kingkobold27 <samuelrempjr@gmail.com>
2025-11-05 16:39:24 -0500

    Add interactive user removal script
    This script prompts the user to remove system users interactively and displays the list of removed users at the end.

---
+++ remove_users.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+users=$(getent passwd | awk -F: '{print $1}')
+removed_users=""
+
+for user in $users; do 
+  echo "User: $user"
+  echo "Removed Users so far: $removed_users"
+  
+  read -p "Do you want to remove this user? [Y/N] " answer
+  
+  if [ "$answer" = "Y" ]; then
+    sudo userdel "$user"
+    removed_users+="$user "
+  elif [ "$answer" = "N" ]; then
+    continue
+  else
+    echo "Invalid input. Please enter Y or N."
+  fi
+done
+
+echo "All removed users: $removed_users"
