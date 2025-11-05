75c3587e8de9c4424d3402ed5d6a172e418fc384
kingkobold27 <samuelrempjr@gmail.com>
2025-11-05 16:11:57 -0500

    Fix syntax errors in user_lockdown.sh

--- user_lockdown.sh
+++ user_lockdown.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-users = $(getent passwd | awk -F:)
+users=$(getent passwd | awk -F: '{print $1}')
+lockedusers=""
 
+for user in $users; do 
+  echo "User: $user"
+  echo "Locked Users so far: $lockedusers"
+  
+  read -p "Do you want to lock this user? [Y/N] " answer
+  
+  if [ "$answer" = "Y" ]; then
+    sudo passwd -l "$user"
+    lockedusers+="$user "
+  elif [ "$answer" = "N" ]; then
+    continue
+  else
+    echo "Huh?"
+  fi
+done
+
+echo "All locked users: $lockedusers"
