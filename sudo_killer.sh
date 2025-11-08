0a959b1c2caa498c12586a2d8dcdb0863a259eec
kingkobold27 <samuelrempjr@gmail.com>
2025-11-08 11:38:38 -0500

    Add user prompt to remove sudo access
    This script prompts for each system user to confirm removal of their sudo access and lists all removed users at the end.

--- sudo_killer.sh
+++ sudo_killer.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+# Get all system users
 users=$(getent passwd | awk -F: '{print $1}')
+removed_users=""
 
+# Loop through each user
+for user in $users; do
+  read -p "Do you want to remove sudo access for '$user'? [Y/N] " answer
+  
+  case "$answer" in
+    [Yy])
+      sudo gpasswd -d "$user" sudo
+      removed_users+="$user "
+      ;;
+    [Nn])
+      continue
+      ;;
+    *)
+      echo "Invalid input. Please enter Y or N."
+      ;;
+  esac
+done
+
+echo
+echo "All removed users: $removed_users"
